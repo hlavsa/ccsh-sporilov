f8b82af50f8023199445aca2e99ebb72c09bbacb
hlavsa <hlavsa.jara@seznam.cz>
2019-06-03 11:03:17 +0200

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root 'welcome#index2'  
 
-  get 'pages/history', to: 'pages#history', as: 'history'
-  get 'pages/offers-index', to: 'pages#offers-index', as: 'offers-index'
-  get 'welcome/photogallery', to: 'welcome#photogallery', as: 'photogallery'
-  get 'pages/oldpeoples', to: 'pages#oldpeoples', as: 'oldpeoples'
+  get '/historie-sporilova', to: 'pages#history', as: 'history'
+  get '/pastoracni-pece', to: 'pages#offers-index', as: 'offers-index'
+  get '/fotogalerie', to: 'welcome#photogallery', as: 'photogallery'
+  get '/domov-pro-seniory', to: 'pages#oldpeoples', as: 'oldpeoples'
 
-  get 'offers/krest', to: 'offers#krest', as: 'krest'
-  get 'offers/domaci', to: 'offers#domaci', as: 'domaci'
-  get 'offers/hodiny', to: 'offers#hodiny', as: 'hodiny'
-  get 'offers/manzelstvi', to: 'offers#manzelstvi', as: 'manzelstvi'
-  get 'offers/pohreb', to: 'offers#pohreb', as: 'pohreb'
-  get 'offers/zpoved', to: 'offers#zpoved', as: 'zpoved'
+  get '/svatost-krest', to: 'offers#krest', as: 'krest'
+  get '/domaci-poboznost', to: 'offers#domaci', as: 'domaci'
+  get '/biblicke-hodiny', to: 'offers#hodiny', as: 'hodiny'
+  get '/svatost-manzelstvi', to: 'offers#manzelstvi', as: 'manzelstvi'
+  get '/pohreb', to: 'offers#pohreb', as: 'pohreb'
+  get '/svatost-zpoved', to: 'offers#zpoved', as: 'zpoved'
   
 end
